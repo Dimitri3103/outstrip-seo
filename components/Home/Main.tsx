62a9d8e45b196eb245d2823ff1d4a158f93f6fe2
Dimitri NSD <it@tenteeglobal.com>
2024-02-09 17:03:38 +0100

    updating front (3)

--- components/Home/Main.tsx
+++ components/Home/Main.tsx
@@ -737,7 +737,7 @@ export default function Main() {
                                 <Box sx={{ display: { xs: "flex" }, justifyContent: "center", alignItems: "center" }}>
                                     <img
                                         alt="site_img"
-                                        src="http://localhost:8005/media/screenshot.png"
+                                        src="https://api.tenteeglobal.com/seo-toolbox/media/screenshot.png"
                                         style={AuthImgStyle}
                                     />
                                 </Box>
